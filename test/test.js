b10800aa382e6d79bef0b00d751e1c22264b7a88
Adrian C Shum <adrian@cshum.com>
2015-03-01 11:11:40 +0800

    Fix test cases

--- test/test.js
+++ test/test.js
@@ -5,44 +5,47 @@ function Clock(){
   this._tick = 'tick';
   this._tock = 'tock';
 }
+
+function base(ctx, next){
+  ctx.logs = ['clock'];
+  next();
+}
+function tick(ctx, next){
+  ctx.logs.push(this._tick);
+  next();
+}
+function tock(ctx, next){
+  ctx.logs.push(this._tock);
+  next();
+}
+function end(ctx, done){
+  ctx.logs.push('done');
+  done(null, ctx.logs);
+}
 ginga(Clock.prototype)
-  .use([
-    function(ctx, next){
-      ctx.logs = ['clock'];
-      next();
-    }, 
-    function(ctx, next){
-      ctx.logs.push(this._tick);
-      next();
-    }
-  ])
-  .use('tock',function(ctx, next){
-    ctx.logs.push(this._tock);
-    next();
-  })
-  .define(['tick','tock'],function(ctx, done){
-    ctx.logs.push('done');
-    done(null, ctx.logs);
- });
+  .define('tick', base, tick, end)
+  .define('tock', base, tick, tock, end);
 
 var clock1 = new Clock();
 var clock2 = new Clock();
 
-clock2.use({
-  'tick': [
-    function(ctx, next){
-      ctx.logs.push('more');
-      next();
-    },
-    function(ctx, next){
-      ctx.logs.push('and more tick');
-      next();
-    }
-  ],
-  'tock': function(next){
+clock2.use(
+  'tick',
+  function(ctx, next){
+    ctx.logs.push('more');
+    next();
+  },
+  function(ctx, next){
+    ctx.logs.push('and more tick');
+    next();
+  }
+);
+clock2.use(
+  'tock',
+  function(ctx, next){
     next('booooom');
   }
-});
+);
 
 tape('ginga middleware', function (t) {
   t.plan(8);
