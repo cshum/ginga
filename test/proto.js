77b25799aea30bde42149db3093b9dbdf4b7c14d
Adrian Shum <me@cshum.com>
2015-11-05 17:45:33 +0800

    add resolver test case

--- test/proto.js
+++ test/proto.js
@@ -1,57 +1,61 @@
 var tape = require('tape')
 var ginga = require('../')
 
-function Clock () {
-  this._tick = 'tick'
-  this._tock = 'tock'
-}
-
-function base (ctx, next) {
-  ctx.logs = ['clock']
-  next()
-}
-function tick (ctx) {
-  // no callback
-  ctx.logs.push(this._tick)
-}
-function tock (ctx, next) {
-  ctx.logs.push(this._tock)
-  next()
-}
-function end (ctx, done) {
-  ctx.logs.push('done')
-  done(null, ctx.logs)
-}
-var C = ginga(Clock.prototype)
-
-var clock1 = new Clock()
-var clock2 = new Clock()
-
-clock2.use(
-  'tick',
-  function (ctx, next) {
-    ctx.logs.push('more')
-    next()
-  },
-  function (ctx, next) {
-    ctx.logs.push('and more tick')
-    next()
-  }
-)
-clock2.use(
-  'tock',
-  function (ctx, next) {
-    next('booooom')
-  }
-)
-
-C.define('tick', end)
-C.define('tock', end)
-C.use('tick', base, tick)
-C.use('tock', base, tick, tock)
-
 tape('ginga prototype', function (t) {
-  t.plan(8)
+  t.plan(12)
+
+  function Clock () {
+    this._tick = 'tick'
+    this._tock = 'tock'
+  }
+  function base (ctx, next) {
+    ctx.logs = ['clock']
+    // async callback
+    setTimeout(next, 10)
+  }
+  function tick (ctx, next) {
+    ctx.logs.push(this._tick)
+    // resolver function
+    next(function (result) {
+      t.equal(result, 167199, 'resolver result')
+    })(null, 167199)
+  }
+  function tock (ctx) {
+    // no next arg
+    ctx.logs.push(this._tock)
+  }
+  function end (ctx, done) {
+    ctx.logs.push('done')
+    done(null, ctx.logs)
+  }
+  var C = ginga(Clock.prototype)
+
+  var clock1 = new Clock()
+  var clock2 = new Clock()
+
+  clock2.use(
+    'tick',
+    function (ctx) {
+      ctx.logs.push('more')
+    },
+    function (ctx) {
+      ctx.logs.push('and more tick')
+    }
+  )
+  clock2.use(
+    'tock',
+    function (ctx, next) {
+      // resolver function err
+      next(function (res) {
+        t.error('resolver called')
+      })('booooom')
+    }
+  )
+
+  C.define('tick', end)
+  C.define('tock', end)
+  C.use('tick', base, tick)
+  C.use('tock', base, tick, tock)
 
   clock1.tick(function (err, res) {
     t.notOk(err, 'no error')
